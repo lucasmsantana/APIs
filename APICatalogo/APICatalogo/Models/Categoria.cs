b47e77f5d41abe2888af9d46f4dbcaa5f82fb97a
Lucas Santana <lucasantana78@gmail.com>
2022-10-28 14:04:05 -0300

    Criando regras utilizando Data Anotations

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
-    public class Categoria
-    {
+[Table("Categoria")]
+public class Categoria
+{
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? Imagem { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
