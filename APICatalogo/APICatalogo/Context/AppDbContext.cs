fafe9774c54e71de4e557a822ba3ed3f3b46bc5f
Lucas Santana <lucasantana78@gmail.com>
2022-10-28 13:44:05 -0300

    Executando a primeira migration

--- APICatalogo/APICatalogo/Context/AppDbContext.cs
+++ APICatalogo/APICatalogo/Context/AppDbContext.cs
@@ -5,7 +5,8 @@ namespace APICatalogo.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base (options)
         { 
         }
 
